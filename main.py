a5027534603886330c156759c6125b9759edb9a9
VivekBhawsar18 <93441216+VivekBhawsar18@users.noreply.github.com>
2021-12-18 17:27:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import random
 
-# Snake Water Gun or Rock Paper Scissors
+
 def gameWin(comp, you):
-    # If two values are equal, declare a tie!
+    
     if comp == you:
         return None
 
-    # Check for all possibilities when computer chose s
+    
     elif comp == 's':
         if you=='w':
             return False
         elif you=='g':
             return True
     
-    # Check for all possibilities when computer chose w
+    
     elif comp == 'w':
         if you=='g':
             return False
         elif you=='s':
             return True
     
-    # Check for all possibilities when computer chose g
+  
     elif comp == 'g':
         if you=='s':
             return False
